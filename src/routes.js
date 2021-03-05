b6439bd08bd8a4f6ee781c9619a0f497aea4a890
Deepansh Bhargava <deepansh940@gmail.com>
2021-03-05 23:25:43 +0530

    Fixed route order

--- src/routes.js
+++ src/routes.js
@@ -6,8 +6,8 @@ import Cart from './views/Cart'
 function Routes() {
   return (
     <Switch>
-      <Route exact path="/" render={props => <Home {...props} />} />
       <Route exact path="/:cart" render={props => <Cart {...props} />} />
+      <Route exact path="/" render={props => <Home {...props} />} />
       <Redirect from="*" to="/notFound" />
     </Switch>
   )
